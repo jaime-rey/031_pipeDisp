27116cb4e06486d15b29c0621fa568f0826ea62f
jaime <jaimereycasado@gmail.com>
2023-05-20 20:08:55 +0200

    ejemplos de pipes 1, first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DatePipeComponent } from './date-pipe/date-pipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatePipeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/date-pipe/date-pipe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-pipe',
+  templateUrl: './date-pipe.component.html',
+  styleUrls: ['./date-pipe.component.css']
+})
+export class DatePipeComponent {
+  fecha: Date = new Date(1988, 3, 15, 12, 30, 15); //15 de Abril del 1988 12:30:15
+}
